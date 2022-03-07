e64d8ff86414c5d2a3486e6cfce08c13d77b0953
Susan Lamberton <kylelamberton1@gmail.com>
2022-03-07 14:50:07 +0000

    added CRUD tests

--- application/models.py
+++ application/models.py
@@ -1,7 +1,14 @@
 from application import db
 
-    
 
+'''
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30))
+    email = db.Column(db.String(150))
+    password = db.Column(db.String(100))
+    movie = db.relationship('Movie', backref='user', lazy=True)
+'''
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     director_name = db.Column(db.String(60))
