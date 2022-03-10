0ef3cf09a2a461109bef47fc6571641f21d51c36
Ubuntu <kylelamberton1@kyle-project-jenkins.55ncilad35sehgodj5m0tvifae.zx.internal.cloudapp.net>
2022-03-10 19:17:57 +0000

    adding setup stage

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "Setup stage"
+
+sudo apt-get update
+sudo apt-get install -y curl jq
+
+curl https://get.docker.com | sudo bash
+
+
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker login --username $DOCKER_HUB_CREDS_USER --password $DOCKER_HUB_CREDS_PSW
