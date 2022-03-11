274b8fc0ca350834101ee9ab72070030423b6636
Ubuntu <kylelamberton1@jenkins-development.lg0jxvtdxx1upmco2hlkpamnhb.zx.internal.cloudapp.net>
2022-03-11 13:44:22 +0000

    testing problems

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 echo "Test stage"
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip3 install pytest flask_testing
+pip3 install -r requirements.txt
+
+mkdir test_reports
+
+python3 -m pytest --cov=application \
+    --cov=frontend/application \
+    --cov-report term-missing \
+    --cov-report xml:test_reports.xml 
+    --junitxml=junit_report.xml
+
+deactivate
+rm -rf venv
