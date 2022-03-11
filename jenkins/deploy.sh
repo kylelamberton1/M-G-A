39a6bd3d987bc836b7dc4f24f5ab29ffac573aea
kylelamberton1 <97620404+kylelamberton1@users.noreply.github.com>
2022-03-11 17:56:59 +0000

    Update deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,3 +1,8 @@
+
 #!/bin/bash
 
 echo "Build deploy"
+
+scp docker-compose.yaml jenkins@swarm-deployment:/home/jenkins/docker-compose.yaml
+
+ssh jenkins@swarm-deployment docker stack deploy --compose-file docker-compose.yaml m-g-a
