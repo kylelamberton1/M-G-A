5147f9d9df0118d0898466b8735aba4b0a09e94d
kylelamberton1 <97620404+kylelamberton1@users.noreply.github.com>
2022-03-11 17:58:58 +0000

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 echo "Test stage"
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip3 install pytest flask_testing
+pip3 install -r requirements.txt
+
+mkdir test_reports
+
+python3 -m pytest --cov=application \
+    
+
+deactivate
+rm -rf venv
