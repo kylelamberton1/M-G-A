a611fbd9e555bd632fab38c638554cb3cdf95f8e
Ubuntu <kylelamberton1@jenkins-development.lg0jxvtdxx1upmco2hlkpamnhb.zx.internal.cloudapp.net>
2022-03-11 12:36:29 +0000

    error fix 2

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,6 +2,6 @@
 
 echo "Build deploy"
 
-
+scp docker-compose.yaml jenkins@swarm-deployment:/home/jenkins/docker-compose.yaml
 
 ssh jenkins@swarm-deployment docker stack deploy --compose-file docker-compose.yaml m-g-a
