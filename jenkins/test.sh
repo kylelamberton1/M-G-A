c904f02312fff39ae894408d7fe4d481a30cfe54
Ubuntu <kylelamberton1@jenkins-development.lg0jxvtdxx1upmco2hlkpamnhb.zx.internal.cloudapp.net>
2022-03-11 13:46:53 +0000

    test problems

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -11,10 +11,7 @@ pip3 install -r requirements.txt
 mkdir test_reports
 
 python3 -m pytest --cov=application \
-    --cov=frontend/application \
-    --cov-report term-missing \
-    --cov-report xml:test_reports.xml 
-    --junitxml=junit_report.xml
+    
 
 deactivate
 rm -rf venv
